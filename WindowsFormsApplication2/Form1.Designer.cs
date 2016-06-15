646a0a26f3af8360519e5a66ed258831fdef913a
yangshuai0711 <yangshuai0711@163.com>
2016-06-15 22:52:01 +0800

    init project

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace com.mocoder.tools.focus_monitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(334, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "当前占用焦点进程";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.textBox1.Location = new System.Drawing.Point(244, 300);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(303, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.DetectUrls = false;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.EnableAutoDragDrop = true;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(784, 403);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 403);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "鼠标焦点监控工具 by alex（QQ807144934）";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
+namespace com.mocoder.tools.focus_monitor
+{
+    public partial class Form1 : Form
+    {
+        private string preStr = "";
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        public static extern IntPtr GetForegroundWindow();//获取当前激活窗口
+
+        [DllImport("user32", SetLastError = true)]
+        public static extern int GetWindowText(
+        IntPtr hWnd, //窗口句柄
+        StringBuilder lpString, //标题
+        int nMaxCount  //最大值
+        );
+
+        [DllImport("user32.dll")]
+        private static extern int GetClassName(
+            IntPtr hWnd, //句柄
+            StringBuilder lpString, //类名
+            int nMaxCount //最大值
+        );
+        [DllImport("user32", EntryPoint = "GetWindowThreadProcessId")]
+        private static extern int GetWindowThreadProcessId(
+            IntPtr hWnd,
+            out int pid
+        );
+
+        public Form1()
+        {
+            InitializeComponent();
+            //this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Opacity = 0.5;
+            timer1.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            IntPtr myPtr = GetForegroundWindow();
+
+            // 窗口标题
+            StringBuilder title = new StringBuilder(255);
+            GetWindowText(myPtr, title, title.Capacity);
+
+            // 窗口类名
+            StringBuilder className = new StringBuilder(256);
+            GetClassName(myPtr, className, className.Capacity);
+            int pid = 0;
+            GetWindowThreadProcessId(myPtr, out pid);
+            string  str =  DateTime.Now.ToShortTimeString() + " ### 标题:" + title.ToString() +" 进程id:"+pid+ " 句柄:" + myPtr.ToString() + " 类:" + className.ToString();
+           
+            if (preStr != myPtr.ToString())
+            {
+               if (richTextBox1.TextLength > richTextBox1.MaxLength)
+               {
+                   richTextBox1.ResetText();
+               }
+                richTextBox1.Text +=str+"\n";
+                preStr = myPtr.ToString();
+           }
+           
+        }
+
+     
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
